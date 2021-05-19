6b98852520469701561d41ee72ab02882c00f337
johanliebert <imadwydad7@gmail.com>
2021-05-19 08:20:19 +0100

    init commit

---
+++ src/main/java/me/imadenigma/armies/Armies.kt
@@ -0,0 +1,19 @@
+package me.imadenigma.armies
+
+import me.lucko.helper.plugin.ap.Plugin
+import org.bukkit.plugin.java.JavaPlugin
+
+@Plugin(
+    name = "Armies",
+    version = "1.12",
+    authors = ["Johan Liebert"]
+)
+class Armies : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
